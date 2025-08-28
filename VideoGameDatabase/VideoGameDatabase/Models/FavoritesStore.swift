23a8631cdf7c712e37620f52eac6d10daa4908b5
Nhung <nguyenthinhung29022004@gmail.com>
2025-08-28 11:13:21 +0700

    Create a Favorites screen using Core Data.

--- VideoGameDatabase/VideoGameDatabase/Models/FavoritesStore.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol FavoritesStore {
-    func isFavorite(gameId: Int) -> Bool
-    func addToFavorites(game: Game, completion: @escaping (Error?) -> Void)
-    func removeFromFavorites(gameId: Int, completion: @escaping (Error?) -> Void)
-    func fetchAll() -> [Game]        // trả về Game để dùng lại UI hiện có
-}
